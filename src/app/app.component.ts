d8039e0e24443636494347b9b13fa941671a500c
aniskatechsopi <aniska.singh@techsopi.com>
2023-11-02 16:06:42 +0530

    this is my template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angulartestproject';
+  onSubmit(contactForm:any) {
+    console.log(contactForm.value);
+  }
 }
